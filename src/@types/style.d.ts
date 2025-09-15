c6150c6040dacac1819855a2b4f941e16f62f13f
JulioCoi <amorim.jc@gmail.com>
2025-09-15 17:09:59 -0300

    estilização de layout padrão

--- src/@types/style.d.ts
+++ src/@types/style.d.ts
@@ -5,7 +5,7 @@ type ThemeType = typeof defaultTheme;
 
 /*
 * cria(sobrescreve, na verdade) tipagem para o módulo Styled-components para tornar-lo acessível em outros arquivos
-* para tirar o erro "Property 'primary' does not exist on type 'DefaultTheme'" no background do botão no arquivo Button.styles
+* retira o aviso de erro "Property 'primary' does not exist on type 'DefaultTheme'" no background do botão no arquivo Button.styles
 */ 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,12 +1,13 @@
 // componente criado para posicionar um header fixo em todas as páginas e evitar que o header fique sendo renderizado novamente em cada pág.
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet /> {/*Componente do react-router-dom para informar onde vai posicionar o conteúdo da página.*/}
-    </div>
+    </LayoutContainer>
   )
 }
